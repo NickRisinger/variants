186d4a44f69ec284d99a42f57196f0b3226ffb86
NickRisinger <boozy044@gmail.com>
2025-01-30 04:00:03 +0300

    feat: add basic server

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
 
 func main() {
 	log.Print("TESTING APP")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":9000"))
 }
